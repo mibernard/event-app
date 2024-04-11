c4d75da8ed96c184b113a2b98f352d88f5ed4a68
Matthew <matthewbernard4@gmail.com>
2024-04-10 18:22:02 -0700

    implement event details page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,18 +5,46 @@ import { connectToDatabase } from '../database';
 import { handleError } from '../utils';
 import User from '../database/models/user.model';
 import Event from '../database/models/event.model';
+import Category from '../database/models/category.model';
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+    .populate({ path: 'category', model: Category, select: '_id name' });
+};
 
 export const createEvent = async ({ event, userId, path }: CreateEventParams) => {
   try {
     await connectToDatabase();
     const organizer = await User.findById(userId);
-    if (!organizer) {
-      throw new Error('Organizer not found');
-    }
-    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId });
+    // if (!organizer) {
+    //   throw new Error('Organizer not found');
+    // }
+    // const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId });
+
+    //made organizer optional in case user doesnt want to register an account (again for demo purposes)
+
+    const eventData = organizer
+      ? { ...event, category: event.categoryId, organizer: userId }
+      : { ...event, category: event.categoryId };
+    const newEvent = await Event.create(eventData);
+
     return JSON.parse(JSON.stringify(newEvent));
   } catch (error) {
     console.log(error);
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+    const event = await populateEvent(Event.findById(eventId));
+    if (!event) {
+      throw new Error('Event not found');
+    }
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
     handleError(error);
   }
 };
